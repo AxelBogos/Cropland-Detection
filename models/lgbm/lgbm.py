9330a949c7f7a3cbf5458021d5ae33ba22c5d817
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-11-25 10:29:09 -0500

    Added flexibility to run with no val set

--- models/lgbm/lgbm.py
+++ models/lgbm/lgbm.py
@@ -1,5 +1,9 @@
 import os
-from models.utils import run_single_experiment
+from models.utils import run_single_experiment, load_data
 import lightgbm as lgbm
+
+name = os.path.basename(__file__).split('.')[0]
+X_train, X_val, y_train, y_val, X_test = load_data(scaler='standard', split_val=True)
 clf = lgbm.LGBMClassifier()
-run_single_experiment(clf=clf, model_name=os.path.basename(__file__).split('.')[0], scaler='standard')
+run_single_experiment(clf=clf, model_name=name, X_train=X_train, y_train=y_train,
+                      X_val=X_val, y_val=y_val, X_test=X_test, use_comet=False)
--- models/utils.py
+++ models/utils.py
@@ -44,12 +44,12 @@ def save_preds(preds: pd.Series, model_name: str, params: dict, metrics: dict) -
         pprint.pprint(params, f)
 
 
-def load_data(scaler: str = "standard") -> tuple:
+def load_data(scaler: str = "standard", split_val=True) -> tuple:
     """Loads teh original dataset, scales it and returns the train-val split.
 
     Args:
         scaler (str, optional): Name of the wanted scaler. Currently "standard" or "minmax". Defaults to "standard".
-
+        split_val (bool, optional): Bool to determine if we split train in train/val.  Defaults to True.
     Returns:
         tuples: X_train, X_val, y_train, y_val, X_test
     """
@@ -83,23 +83,34 @@ def load_data(scaler: str = "standard") -> tuple:
         X_test[columns_to_normalize] = df_temp
 
     X, y = train.drop(columns=["LABELS"]), train["LABELS"]
-    X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=RANDOM_SEED)
-    return X_train, X_val, y_train, y_val, X_test
+    if split_val:
+        X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=RANDOM_SEED)
+        return X_train, X_val, y_train, y_val, X_test
+    else:
+        return X, y, X_test
 
 
-def run_single_experiment(
-    clf, model_name, scaler: str = None, use_comet: bool = False, do_save_preds=True
-):
+def run_single_experiment(clf, model_name: str, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame,
+                          X_val: pd.DataFrame = None, y_val: pd.Series = None, use_comet: bool = False, do_save_preds=True):
     """Runs a single experiment with the provided classifier.
 
     Args:
         clf  : classifier. Assumed to have a fit/predict pattern.
         model_name (str): Name (type) of the model. ex: 'random_forest'.
-        scaler (str, optional): Type of scaler to be used when loading data. See 'load_data()'. Defaults to None.
+        X_train (pd.Dataframe): Train set features
+        y_train (pd.Series): Train set labels
+        X_val (pd.Dataframe): val set features
+        y_val (pd.Series): val set labels
+        X_test (pd.Dataframe, optional): Test set features
         use_comet (bool, optional): Use comet.ml logging API. Defaults to False.
         do_save_preds (bool, optional): Bool to decide whehter the predictions on X_test are saved as a csv in data/preds/... Defaults to True.
     """
-    if use_comet:
+    with_val = X_val and y_val
+    if use_comet and not with_val:
+        print('Validation set is necessary to log metrics to Comet')
+        return
+
+    if use_comet and with_val:
         exp = Experiment(
             project_name="data-challenge-2",
             workspace="ift6390-datachallenge-2",
@@ -107,33 +118,35 @@ def run_single_experiment(
             api_key=os.environ.get("COMET_API"),
         )
 
-    X_train, X_val, y_train, y_val, X_test = load_data(scaler)
     params = clf.get_params()
     clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_val)
 
-    f1 = f1_score(y_val, y_pred)
-    accuracy = accuracy_score(y_val, y_pred)
-    precision = precision_score(y_val, y_pred)
-    recall = recall_score(y_val, y_pred)
+    if with_val:
+        y_pred = clf.predict(X_val)
 
-    print(f"Accuracy: {accuracy:.4f} \t F1: {f1:.4f}")
+        f1 = f1_score(y_val, y_pred)
+        accuracy = accuracy_score(y_val, y_pred)
+        precision = precision_score(y_val, y_pred)
+        recall = recall_score(y_val, y_pred)
 
-    # these will be logged to your sklearn-demos project on Comet.ml
-    params = {
-        "random_state": RANDOM_SEED,
-        "model_type": model_name,
-        "scaler": "standard scaler",
-        "param_grid": str(params),
-        "stratify": True,
-    }
+        print(f"Accuracy: {accuracy:.4f} \t F1: {f1:.4f}")
 
-    metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
-    if use_comet:
-        exp.log_dataset_hash(X_train)
-        exp.log_parameters(params)
-        exp.log_metrics(metrics)
-        exp.add_tag(model_name)
+        # these will be logged to your sklearn-demos project on Comet.ml
+        params = {
+            "random_state": RANDOM_SEED,
+            "model_type": model_name,
+            "scaler": "standard scaler",
+            "param_grid": str(params),
+            "stratify": True,
+        }
+
+        metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
+        if use_comet:
+            exp.log_dataset_hash(X_train)
+            exp.log_parameters(params)
+            exp.log_metrics(metrics)
+            exp.add_tag(model_name)
+
     test_preds = clf.predict(X_test)
     if do_save_preds:
         save_preds(test_preds, model_name, params, metrics)
